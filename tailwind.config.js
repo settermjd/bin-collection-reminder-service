9554bdf3d891b74fbd592412a6285c00a66ca708
Matthew Setter <matthew@matthewsetter.com>
2023-04-04 11:13:01 +0200

    Update the styling of the site templates
    This change continues refining the style of the site pages so that they
    take on an ever more professional appearance. The most amount of work
    has gone in to the page header and then, to a lesser extent, the footer.
    Previously, they were quite flat, but now they have a better appearance,
    imho. I think that it can often be the little touches that make an app
    really shine.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.twig"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bin-pattern': "url('/images/backgrounds/bin-background.png')",
+        'bin-pattern-200': "url('/images/backgrounds/bin-background-200.png')",
+        'bin-pattern-100': "url('/images/backgrounds/bin-background-100.png')",
+        'bin-pattern-100-50o': "url('/images/backgrounds/bin-background-100-50.png')",
+        'bin-pattern-100-25o': "url('/images/backgrounds/bin-background-100-25.png')"
+      }
+    },
   },
   plugins: [],
 }
