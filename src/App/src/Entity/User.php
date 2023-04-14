ede59dc0cae57e20ad4395c707e728e1ee0926f5
Matthew Setter <matthew@matthewsetter.com>
2023-04-14 14:55:30 +0200

    Add full name field to the User entity and add covering tests
    The full name field is being added as it allows for further, optional,
    personalisation without being too intrusive.

--- src/App/src/Entity/User.php
+++ src/App/src/Entity/User.php
@@ -20,6 +20,9 @@ class User
     #[GeneratedValue(strategy: 'IDENTITY')]
     private ?int $id;
 
+    #[Column(name: 'full_name', type: Types::STRING, length: 100)]
+    private ?string $fullName;
+
     #[Column(name: 'suburb', type: Types::STRING, length: 40)]
     private ?string $suburb;
 
@@ -28,4 +31,39 @@ class User
 
     #[Column(name: 'mobile', type: Types::STRING, length: 14, unique: true, nullable: true)]
     private ?string $mobile;
+
+    public function __construct(?string $suburb, ?string $email, ?string $mobile, ?string $fullName, ?int $id = null)
+    {
+        $this->id = $id;
+        $this->suburb = $suburb;
+        $this->email = $email;
+        $this->mobile = $mobile;
+        $this->fullName = $fullName;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id ?? null;
+    }
+
+    public function getSuburb(): string
+    {
+        return $this->suburb ?? '';
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email ?? '';
+    }
+
+    public function getMobile(): string
+    {
+        return $this->mobile ?? '';
+    }
+
+    public function getFullName(): ?string
+    {
+        return $this->fullName;
+    }
+
 }
