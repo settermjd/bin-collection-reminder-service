45af99abd1f65c290275ec64dbeb13d6e5424456
Matthew Setter <matthew@matthewsetter.com>
2023-04-18 12:49:40 +0200

    Minor code clean up

--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 // Delegate static file requests back to the PHP built-in webserver
+use Mezzio\Application;
+use Psr\Container\ContainerInterface;
+
 if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
@@ -27,11 +30,11 @@ $dotenv->required([
  * Self-called anonymous function that creates its own scope and keeps the global namespace clean.
  */
 (function () {
-    /** @var \Psr\Container\ContainerInterface $container */
+    /** @var ContainerInterface $container */
     $container = require 'config/container.php';
 
-    /** @var \Mezzio\Application $app */
-    $app = $container->get(\Mezzio\Application::class);
+    /** @var Application $app */
+    $app = $container->get(Application::class);
     $factory = $container->get(\Mezzio\MiddlewareFactory::class);
 
     // Execute programmatic/declarative middleware pipeline and routing
